069e1c714885a8bb3d898e386e7eeca69da611e3
Cristi ANGHELUTA <cristinel.angheluta@gmail.com>
2021-12-26 19:54:56 +0200

    add UserInfoForm as an inline React component when using withEditableUser HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-import { withEditableUser } from './withEditableUser'
 import { UserInfoForm } from './UserInfoForm'
-
-const UserInfoWithEditor = withEditableUser(UserInfoForm, '345');
 
 function App() {
 	return (
-		<UserInfoWithEditor />
+		<UserInfoForm/>
 	);
 }
 
--- src/UserInfoForm.js
+++ src/UserInfoForm.js
@@ -1,4 +1,29 @@
-export const UserInfoForm = (props) => {
-
-  return <></>
-}
+import { withEditableUser } from './withEditableUser'
+/* defining an inline component
+  (props) => {
+    return <></>
+  }
+*/
+export const UserInfoForm = withEditableUser(({user, onChangeUser, onResetUser, onSaveUser}) => {
+  const {name, age, hairColor} = user || {}
+  return user
+    ? <>
+      <form onSubmit={e => e.preventDefault()}>
+        <label>Name:
+          <input type="text" value={name} onChange={e => onChangeUser({name: e.target.value})}/>
+        </label>
+        <div/>
+        <label>Age:
+          <input type="number" value={age} onChange={e => onChangeUser({age: Number(e.target.value)})}/>
+        </label>
+        <div/>
+        <label>Hair Color:
+          <input type="text" value={hairColor} onChange={e => onChangeUser({hairColor: e.target.value})}/>
+        </label>
+        <div/>
+        <button onClick={onSaveUser}>Save</button>
+        <button onClick={onResetUser}>Reset</button>
+      </form>
+    </>
+    : <p>Loading...</p>
+}, '123')
